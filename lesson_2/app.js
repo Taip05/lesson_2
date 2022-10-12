b355c24a3e30aaa87f57f96361eeddf6cb134bb0
Taip05 <115586426+Taip05@users.noreply.github.com>
2022-10-12 15:35:22 +0300

    Add files via upload

---
+++ lesson_2/app.js
@@ -0,0 +1,11 @@
+// обьекты
+let user = {
+    name: "John",
+    age: "27",
+    adress: "new York",
+};
+    console.log(user);
+
+    //массивы
+let phones = ["iphone","android", "Los", "samsung", "Redmi"];
+console.log(phones) ;
